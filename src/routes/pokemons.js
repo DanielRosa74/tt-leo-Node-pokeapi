416cfb81669e8b702824c46c1b59d7e6a53839eb
Daniel Rosa <daniel.c.rosa@gmail.com>
2021-02-08 12:55:45 -0300

    Leos fixes

--- src/routes/pokemons.js
+++ src/routes/pokemons.js
@@ -37,115 +37,125 @@ pokemonsRouter.get('/:name', async (req, res) => {
 })
 
 pokemonsRouter.post('/create', async (req, res) => {
-    const { types: requestTypes, moves: requestMoves } = req.body
-    
-    const promises = []
+  const { types: requestTypes, moves: requestMoves } = req.body
 
-    requestTypes.forEach(type => {
-        promises.push(Types.findOne({ name: type }))
-    })
+  const typesPromises = []
+  const movesPromises = []
 
-    requestMoves.forEach(move => {
-        promises.push(Moves.findOne({ name: move }))
-    })
+  requestTypes.forEach(type => {
+    typesPromises.push(Types.findOne({ name: type }))
+  })
 
-    Promise.allSettled(promises).then(results => {
-        const types = []
-        const moves = []
+  requestMoves.forEach(move => {
+    movesPromises.push(Moves.findOne({ name: move }))
+  })
 
-        results.forEach(({ status, value }) => {
-            if(status === 'fulfilled' && value) {
-                types.push(value._id)
-                moves.push(value._id)
-            }
-        })
+  const newPokemon = new Pokemons({ ...req.body })
 
-        const newPokemon = new Pokemons( { ...req.body, types, moves })
+  const types = []
 
-        newPokemon.save(err => {
-            if(err){
-                return res.status(500).json({
-                    success: false,
-                    message: 'Erro interno do servidor',
-                    error: err
-                })
-            }
-            return res.status(201).json({
-                success: true,
-                pokemon: newPokemon,
-            })
-        })
-    })
+  const typesInfo = await Promise.allSettled(typesPromises)
+
+  typesInfo.forEach(type => {
+    if (type.status === 'fulfilled') {
+      types.push(type.value)
+    }
+  })
+
+  const moves = []
+
+  const movesInfo = await Promise.allSettled(movesPromises)
+
+  movesInfo.forEach(move => {
+    if (move.status === 'fulfilled') {
+      moves.push(move.value)
+    }
+  })
+
+  newPokemon.types = types
+  newPokemon.moves = moves
+
+  newPokemon.save(err => {
+    if (err) {
+      return res.status(500).json({ success: false, error: err })
+    }
+  })
+
+  return res.status(201).json({ success: true, pokemon: newPokemon })
 })
 
-pokemonsRouter.post('/update', async (req, res) => {
-    const { name } = req.body
+pokemonsRouter.patch('/update', async (req, res) => {
+  const { name } = req.body
 
-    if(!name){
-        res.status(400).json({
-            success: false,
-            message: "O nome é obrigatório"
-        })
-        return
-    }
-
-    const pokemon = await Pokemons.findOne({ name }).populate('types').populate('moves')
-    
-    if(req.body.types) {
-        const promises = []
-        req.body.types.forEach(type => {
-            promises.push(Types.findOne({ name: type }))
-        })
-        
-        Promise.allSettled(promises).then(results => {
-            const types = []
-            
-            results.forEach(({ status, value }) => {
-                if(status === 'fulfilled' && value) {
-                    types.push(value._id)
-                }
-            })
-            pokemon.types = pokemon.types.concat(types)
-        })
-    }
-
-    if(req.body.moves) {
-        const promises = []
-
-        req.body.moves.forEach(move => {
-            promises.push(Moves.findOne({ name: move }))
-        })
-        
-        Promise.allSettled(promises).then(results => {
-            const moves = []
-
-            results.forEach(({ status, value }) => {
-                if(status === 'fulfilled' && value) {
-                    moves.push(value._id)
-                }
-            })
-            pokemon.moves = pokemon.moves.concat(moves)
-        })
-    }
-
-    if(req.body.height) pokemon.height = req.body.height
-    if(req.body.weight) pokemon.weight = req.body.weight
-    if(req.body.image) pokemon.image = req.body.image
-    pokemon.updatedAt = new Date()
-    
-    await pokemon.save(err => {
-        if(err){
-            return res.status(500).json({
-                success: false,
-                message: 'Erro interno do servidor',
-                error: err
-            })
-        }
-        res.status(200).json({
-            success: true,
-            pokemon
-        })
+  if (!name) {
+    res.status(400).json({
+      success: false,
+      message: 'O nome é obrigatório',
     })
+    return
+  }
+
+  const pokemon = await Pokemons.findOne({ name })
+    .populate('types')
+    .populate('moves')
+
+  if (req.body.types) {
+    const promises = []
+    req.body.types.forEach(type => {
+      promises.push(Types.findOne({ name: type }))
+    })
+
+    const types = []
+
+    const typesInfo = await Promise.allSettled(promises)
+
+    typesInfo.forEach(type => {
+      if (type.status === 'fulfilled') {
+        types.push(type.value)
+      }
+    })
+
+    pokemon.types = types
+  }
+
+  if (req.body.moves) {
+    const promises = []
+    req.body.moves.forEach(move => {
+      promises.push(Moves.findOne({ name: move }))
+    })
+
+    const moves = []
+
+    const movesInfo = await Promise.allSettled(promises)
+
+    movesInfo.forEach(move => {
+      if (move.status === 'fulfilled') {
+        moves.push(move.value)
+      }
+    })
+
+    pokemon.moves = moves
+  }
+
+  if (req.body.height) pokemon.height = req.body.height
+  if (req.body.weight) pokemon.weight = req.body.weight
+  if (req.body.image) pokemon.image = req.body.image
+  pokemon.updatedAt = new Date()
+
+  pokemon.save(err => {
+    if (err) {
+      return res.status(500).json({
+        success: false,
+        message: 'Erro interno do servidor',
+        error: err,
+      })
+    }
+  })
+
+  return res.status(200).json({
+    success: true,
+    pokemon,
+  })
 })
 
 pokemonsRouter.delete('/delete', async (req, res) => {
